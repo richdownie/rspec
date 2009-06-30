b6934263a43d93df722dc7a9a13013a3b9ba7157
Rich Downie <rich@visualcv.com>
2009-06-30 14:12:05 -0400

    Chapter 6

--- features/step_definitions/codebreaker.rb
+++ features/step_definitions/codebreaker.rb
@@ -1,12 +1,34 @@
-Given /I am not yet playing/ do
+def messenger
+  @messenger ||= StringIO.new
 end
 
-When /I start a new game/ do
-  @messenger = StringIO.new
-  game = Codebreaker::Game.new(@messenger)
-  game.start
+def game
+  @game ||= Codebreaker::Game.new(messenger)
 end
 
-Then /the game should say "([^\"]*)"/ do |message|
-  @messenger.string.split("\n").should include(message)
+def messages_should_include(message)
+  messenger.string.split("\n").should include(message)
+end
+
+Given /^I am not yet playing$/ do
+end
+
+Given /^the secret code is (. . . .)$/ do |code|
+  game.start(code.split)
+end
+
+When /^I guess (. . . .)$/ do |code|
+  game.guess(code.split)
+end
+
+When /^I start a new game$/ do
+  game.start(%w[r g y c])
+end
+
+Then /^the game should say "([^\"]*)"$/ do |message|
+  messages_should_include(message)
+end
+
+Then /^the mark should be (.*)$/ do |mark|
+  messages_should_include(mark)
 end
